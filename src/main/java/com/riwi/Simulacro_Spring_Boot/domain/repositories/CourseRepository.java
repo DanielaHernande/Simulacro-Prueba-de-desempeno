6654cb68e46860dba31345b4acdd34e06f19e667
DanielaHernande <danielajimenezhernandez6@gmail.com>
2024-05-28 18:03:52 -0500

    Feat: Creation of the repositories

--- src/main/java/com/riwi/Simulacro_Spring_Boot/domain/repositories/CourseRepository.java
+++ src/main/java/com/riwi/Simulacro_Spring_Boot/domain/repositories/CourseRepository.java
@@ -11,10 +11,10 @@ import com.riwi.Simulacro_Spring_Boot.domain.entities.Course;
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long>{
 
-    @Query(value = "select * from courses")
-    public List<Course> findByFirstname();
+    // Para buscar por el nombre del curso
+    @Query(value = "select c from courses c where s.name like :courseName")
+    public List<Course> findByCourseNameContaining(String courseName);
 
 }
-
 
 // https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
