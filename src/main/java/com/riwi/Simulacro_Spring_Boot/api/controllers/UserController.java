9df19240313d2365bfbadd23ed1fe5e45f55a950
DanielaHernande <danielajimenezhernandez6@gmail.com>
2024-05-29 18:03:40 -0500

    Feat: Creation controllers

--- src/main/java/com/riwi/Simulacro_Spring_Boot/api/controllers/UserController.java
+++ src/main/java/com/riwi/Simulacro_Spring_Boot/api/controllers/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,11 +32,20 @@ public class UserController {
     @GetMapping
     public ResponseEntity<Page<UserResp>> getll(
         @RequestParam(defaultValue = "1") int page,
-        @RequestParam(defaultValue = "2") int size
+        @RequestParam(defaultValue = "10") int size
         ) {
         return ResponseEntity.ok(this.userService.getAll(page - 1, size));
     }
 
+    // Buscar por id
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<UserResp> getId(
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(this.userService.get(id));
+    }
+
+    // Crear
     @PostMapping
     public ResponseEntity<UserResp> create(
             @Validated @RequestBody UserReq request) {
