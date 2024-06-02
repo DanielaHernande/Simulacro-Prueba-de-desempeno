7dad0bc1ac64dc5e68886d06edddf8540bd6f2c6
Daniela Jimenez Hernandez <danielaysantiagotqm2007@gmail.com>
2024-06-01 22:47:07 -0500

    Feat: Create controller

--- src/main/java/com/riwi/Simulacro_Spring_Boot/api/controllers/EnrollmentController.java
+++ src/main/java/com/riwi/Simulacro_Spring_Boot/api/controllers/EnrollmentController.java
@@ -3,11 +3,16 @@ package com.riwi.Simulacro_Spring_Boot.api.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.riwi.Simulacro_Spring_Boot.api.dto.request.CourseReq;
+import com.riwi.Simulacro_Spring_Boot.api.dto.request.EnrollmentReq;
 import com.riwi.Simulacro_Spring_Boot.api.dto.response.CourseResp;
 import com.riwi.Simulacro_Spring_Boot.api.dto.response.EnrollmentResp;
 import com.riwi.Simulacro_Spring_Boot.infrastructure.abstract_services.ICourseService;
@@ -31,6 +36,14 @@ public class EnrollmentController {
             @RequestParam(defaultValue = "10") int size
             ) {
         return ResponseEntity.ok(this.enrollmentsService.getAll(page, size));
+    }
+
+    // Crear 
+    @PostMapping
+    public ResponseEntity<EnrollmentResp> create(
+            @Validated @RequestBody EnrollmentReq request) {
+
+        return ResponseEntity.ok(this.enrollmentsService.create(request));
     }
     
 }
