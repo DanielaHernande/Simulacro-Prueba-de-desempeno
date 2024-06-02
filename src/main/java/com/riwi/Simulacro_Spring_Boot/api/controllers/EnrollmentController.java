1def4516ee0c7555804a2ff5e20e8b6b3c95a8ca
Daniela Jimenez Hernandez <danielaysantiagotqm2007@gmail.com>
2024-06-01 23:09:38 -0500

    Feat: Create controller

--- src/main/java/com/riwi/Simulacro_Spring_Boot/api/controllers/EnrollmentController.java
+++ src/main/java/com/riwi/Simulacro_Spring_Boot/api/controllers/EnrollmentController.java
@@ -4,18 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.riwi.Simulacro_Spring_Boot.api.dto.request.CourseReq;
 import com.riwi.Simulacro_Spring_Boot.api.dto.request.EnrollmentReq;
-import com.riwi.Simulacro_Spring_Boot.api.dto.response.CourseResp;
 import com.riwi.Simulacro_Spring_Boot.api.dto.response.EnrollmentResp;
-import com.riwi.Simulacro_Spring_Boot.infrastructure.abstract_services.ICourseService;
 import com.riwi.Simulacro_Spring_Boot.infrastructure.abstract_services.IEnrollmentsService;
 
 import lombok.AllArgsConstructor;
@@ -38,12 +38,37 @@ public class EnrollmentController {
         return ResponseEntity.ok(this.enrollmentsService.getAll(page, size));
     }
 
+    // Obtener por id
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<EnrollmentResp> get(
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(this.enrollmentsService.get(id));
+    }
+
     // Crear 
     @PostMapping
     public ResponseEntity<EnrollmentResp> create(
             @Validated @RequestBody EnrollmentReq request) {
 
         return ResponseEntity.ok(this.enrollmentsService.create(request));
+    }
+
+    // Actualizar
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<EnrollmentResp> update(
+            @Validated @RequestBody EnrollmentReq request,
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(this.enrollmentsService.update(request, id));
+    }
+
+    // Eliminar 
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        
+        this.enrollmentsService.delete(id);
+        return ResponseEntity.noContent().build();
     }
     
 }
